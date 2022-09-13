b4bf3c9c84f5ba0ce903dadd8867f1ebb9af0870
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-09-13 10:05:51 -0300

    Adicionando os 'Acessores do Javascript', get e o set para atribuir e acessar.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
+//---------Maneira considerada correta
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
@@ -18,8 +19,12 @@ contaCorrenteRicardo.depositar(500);
 const conta2 = new ContaCorrente();
 conta2.cliente = cliente2;
 conta2.agencia = 1002;
+//----------outra maneira de popular os atributos
+//conta2.cliente = new Cliente();
+// conta2.cliente.nome = "Alice";
+// conta2.cliente.cpf = 88822233309;
+// conta2.agencia = 1002;
 
 contaCorrenteRicardo.transferir(200, conta2);
 
-console.log(conta2);
-console.log(contaCorrenteRicardo);
+console.log(conta2.saldo);
