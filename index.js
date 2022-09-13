d35fcc53f730b79f3ac7a4a412584efb32cbd8c0
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-09-13 11:01:41 -0300

    Adicionando os constructores dass classes.

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
 import{Cliente} from "./Cliente.js"
 import{ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Ricardo";
-cliente1.cpf = 11122233309;
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente2 = new Cliente("Alice", 88822233309);
 
-//---------Maneira considerada correta
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+        /* Contrução antiga
+        const cliente1 = new Cliente();
+        cliente1.nome = "Ricardo";
+        cliente1.cpf = 11122233309;
 
+        //---------Maneira considerada correta
+        const cliente2 = new Cliente();
+        cliente2.nome = "Alice";
+        cliente2.cpf = 88822233309;
+        */
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.cliente = cliente1;
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+
+        /*
+        contaCorrenteRicardo.agencia = 1001;
+        contaCorrenteRicardo.cliente = cliente1;
+
+        */
 contaCorrenteRicardo.depositar(500);
 
-const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
-conta2.agencia = 1002;
+const conta2 = new ContaCorrente(cliente2, 1002);
+
+        /*
+        conta2.cliente = cliente2;
+        conta2.agencia = 1002;
+        */
+
 //----------outra maneira de popular os atributos
 //conta2.cliente = new Cliente();
 // conta2.cliente.nome = "Alice";
@@ -27,4 +39,4 @@ conta2.agencia = 1002;
 
 contaCorrenteRicardo.transferir(200, conta2);
 
-console.log(conta2.saldo);
+console.log(conta2);
