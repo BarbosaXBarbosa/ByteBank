a51ebab8f1503167e0771024563c3ae3a343998d
João Victor Barbosa <87194640+BarbosaXBarbosa@users.noreply.github.com>
2022-09-13 11:07:49 -0300

    Adicionando construtores das classes.

--- Cliente.js
+++ Cliente.js
@@ -1,4 +1,15 @@
 export class Cliente{
     nome;
-    cpf;
+    _cpf;
+
+    get cpf(){
+        return this._cpf;
+    }
+
+    //Com este construtor a a gente consegue fazer uma propriedade que só vai ser atribuida no momento de criação
+    // e depois nunca mais pode ser atribuida.
+    constructor(nome, cpf){
+        this.nome = nome;
+        this._cpf = cpf;
+    }
 }
